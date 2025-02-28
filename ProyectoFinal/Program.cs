b84c9f5474e64f6122e20bd35f726995124f860b
Iyertorr <torresjaramilloiyersmith@gamil.com>
2025-02-28 07:37:10 -0500

    actualizacion de los proyectos, debo arregalr el registro y la zona de juegos

--- ProyectoFinal/Program.cs
+++ ProyectoFinal/Program.cs
@@ -34,7 +34,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Html}/{action=Principal}/{id?}"
+    pattern: "{controller=Html}/{action=Index}/{id?}"
 );
 
 app.Run();
