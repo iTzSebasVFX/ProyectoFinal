3722211b29c8bcf87a44ead702c67403e812a49a
Iyertorr <torresjaramilloiyersmith@gamil.com>
2025-03-04 08:31:16 -0500

    hola de nuevo

--- ProyectoFinal/Program.cs
+++ ProyectoFinal/Program.cs
@@ -35,7 +35,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Html}/{action=Principal}/{id?}"
+    pattern: "{controller=Html}/{action=Index}/{id?}"
 );
 
 app.Run();
