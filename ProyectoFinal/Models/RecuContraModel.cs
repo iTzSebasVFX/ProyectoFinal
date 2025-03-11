8302e4014c5c9d8d8442f4e505e36053b3cf80d2
iTzSebasVFX <iTzSebasVFX>
2025-03-11 13:15:10 -0500

    Recuperacion de contraseña implementado

---
+++ ProyectoFinal/Models/RecuContraModel.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoFinal.Models
+{
+    public class RecuContra
+    {
+        [Required(ErrorMessage = "Porfavor ingrese su Correo Electronico")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Porfavor ingrese su clave")]
+        public int clave { get; set; } = 0;
+
+        [Required(ErrorMessage = "Debe ingresar la nueva contraseña")]
+        [StringLength(12, MinimumLength = 6, ErrorMessage = "La contraseña debe tener entre 6 y 12 palabras")]
+        public string? contrasena { get; set; }
+
+        [Required(ErrorMessage = "Porfavor confirma tu constraseña")]
+        [Compare("contrasenaValidator", ErrorMessage = "Las contraseñas no coinciden")]
+        public string? contrasenaValidator { get; set; }
+    }
+}
