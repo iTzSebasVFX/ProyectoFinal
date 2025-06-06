c4492455d42acbda6ef94603281b1a3b913dd320
ItzSebasVFX <juansebastian320519@gmail.com>
2025-06-06 11:03:06 -0500

    Seee

--- ProyectoFinal/Controllers/ChatControllers.cs
+++ ProyectoFinal/Controllers/ChatControllers.cs
@@ -1,10 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using ProyectoFinal.Models;
 
 namespace ChatSignalR.controller
 {
     [SessionValidator]
     public class ChatController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public ChatController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public static Dictionary<int, string> Lista = new Dictionary<int, string>{
             {1, "Musica"},
             {2, "Juegos"},
@@ -21,10 +32,33 @@ namespace ChatSignalR.controller
             return View("chat", idChat);
         }
 
-        public IActionResult Invitar(int itemid)
+        public IActionResult CrearRoom()
         {
-            Console.WriteLine(itemid);
-            return View("NewRoom");
+            var CorreoUsuario = HttpContext?.Session.GetString("CorreoUsuario");
+            var SearchUser = _context.Usuarios.FirstOrDefault(u => u.correoElectronico == CorreoUsuario);
+            if (SearchUser != null)
+            {
+                var datos = new RoomModel
+                {
+                    RoomName = null,
+                    CreadorName = SearchUser.id.ToString()
+                };
+                return View("NewRoom", datos);
+            }
+            TempData["Error"] = "Error en el intento de creacion";
+            return RedirectToAction("ListaChats", "Chat");
+        }
+
+        [HttpPost]
+        public IActionResult InsertarRoom(RoomModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["Error"] = "Datos Incorrectos";
+                return RedirectToAction("CrearRoom");
+            }
+            Console.WriteLine($"Id: + {model.Id} + RoomName: + {model.RoomName} + CreadorName: + {model.CreadorName} + fechaCreacion: + {model.fechaCreacion}");
+            return RedirectToAction("CrearRoom", "Chat");
         }
     }
 }
